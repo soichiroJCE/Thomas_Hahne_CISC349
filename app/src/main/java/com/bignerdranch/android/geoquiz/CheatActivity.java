38547250da8f314e09f256c630a004f1c3e1faae
Thomas <thomasdthahne@gmail.com>
2025-01-19 14:40:15 -0500

    Added GeoQuiz

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -0,0 +1,51 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheatActivity extends AppCompatActivity
+{
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswer.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (mAnswerIsTrue)
+                {
+                    mAnswerTextView.setText(R.string.true_button);
+                }
+                else
+                {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("result", "Hello from Cheat Activity");
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+        Intent i = getIntent();
+        String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+    }
+}
